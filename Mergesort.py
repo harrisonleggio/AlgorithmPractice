dcc1e5c30168b7f4b9e6897edc27b3cfb7b53505
harrisonleggio <harrisonleggio@my.uri.edu>
2016-03-26 11:05:18 -0400

    Added comments

--- Mergesort.py
+++ Mergesort.py
@@ -1,14 +1,15 @@
-from heapq import merge
+from heapq import merge             # import merge (python 2.6(?) or later)
 
+# mergesort function
 def mergesort(m):
-    if len(m) <= 1:
+    if len(m) <= 1:                 # returns given array if length is 1
         return m
 
-    middle = len(m) // 2
-    left = m[:middle]
-    right = m[middle:]
+    middle = len(m) // 2            # finds middle point of array
+    left = m[:middle]               # left array takes all numbers from left to midpoint
+    right = m[middle:]              # right array takes all numbers from midpoint to end
 
-    left = mergesort(left)
-    right = mergesort(right)
+    left = mergesort(left)          # recursive call on left
+    right = mergesort(right)        # recursive call on right
 
-    return list(merge(left, right))
+    return list(merge(left, right))     # merge both halves together (merge takes care of sorting)
