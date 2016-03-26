8ee7eaa663ea5fb207d707b700d1d42b1ce99269
harrisonleggio <harrisonleggio@my.uri.edu>
2016-03-25 23:02:40 -0400

    Added comments

--- Quicksortv2.py
+++ Quicksortv2.py
@@ -1,19 +1,20 @@
+# quicksort function
 def quicksort(a):
-    less = []
-    more = []
-    pivotList = []
-    if len(a) <= 1:
+    less = []               # array for #'s less than pivot
+    more = []               # array for #'s greater than pivot
+    pivotList = []          # array for #'s = pivot
+    if len(a) <= 1:         # return original array if len = 1
         return a
     else:
-        pivot = a[0]
+        pivot = a[0]                    # start with first element as pivot
         for i in a:
-            if i < pivot:
+            if i < pivot:               # if # < pivot add to less array
                 less.append(i)
-            elif i > pivot:
+            elif i > pivot:             # if # > pivot add to more array
                 more.append(i)
-            else:
+            else:                       # add pivot + duplicates to pivot array
                 pivotList.append(i)
-        less = quicksort(less)
-        more = quicksort(more)
-        return less + pivotList + more
+        less = quicksort(less)          # recursively sort less array
+        more = quicksort(more)          # recursively sort more array
+        return less + pivotList + more  # add 3 arrays together and return 1 array
 
